3413781a4c461c9e91cb249f5878161d12d4d546
Ain Grace <qnicat32@gmail.com>
2024-11-28 13:07:51 +0400

    Update Message and refactor

--- src/main/java/core/project/messaging/application/controller/UserSessionHandler.java
+++ src/main/java/core/project/messaging/application/controller/UserSessionHandler.java
@@ -1,10 +1,11 @@
 package core.project.messaging.application.controller;
 
+import core.project.messaging.application.dto.Message;
 import core.project.messaging.application.service.UserSessionService;
 import core.project.messaging.domain.value_objects.Username;
+import core.project.messaging.infrastructure.config.application.MessageDecoder;
+import core.project.messaging.infrastructure.config.application.MessageEncoder;
 import core.project.messaging.infrastructure.config.security.JwtUtility;
-import core.project.messaging.infrastructure.utilities.containers.Result;
-import core.project.messaging.infrastructure.utilities.web.WSUtilities;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
@@ -14,9 +15,12 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
-import java.util.Objects;
+import java.util.Optional;
 
-@ServerEndpoint("/user-session")
+import static core.project.messaging.infrastructure.utilities.web.WSUtilities.closeSession;
+import static core.project.messaging.infrastructure.utilities.web.WSUtilities.sendMessage;
+
+@ServerEndpoint(value = "/user-session", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class UserSessionHandler {
 
@@ -25,53 +29,31 @@ public class UserSessionHandler {
 
     @OnOpen
     public final void onOpen(Session session) {
-        final Result<JsonWebToken, Throwable> jwt = jwtUtility.extractJWT(session);
-        if (validateToken(session, jwt)) {
-            return;
-        }
-
-        final Username username = new Username(jwt.value().getName());
-        userSessionService.handleOnOpen(session, username);
+        validateToken(session)
+                .ifPresent(token -> userSessionService.handleOnOpen(session, new Username(token.getName())));
     }
 
     @OnMessage
-    public final void onMessage(Session session, String message) {
-        if (Objects.isNull(message) || message.isBlank()) {
-            WSUtilities.sendMessage(session, "Message is required.");
-            return;
-        }
-
-        if (message.length() > 255) {
-            WSUtilities.sendMessage(session, "This message is to long.");
-            return;
-        }
-
-        final Result<JsonWebToken, Throwable> jwt = jwtUtility.extractJWT(session);
-        if (validateToken(session, jwt)) {
-            return;
-        }
-
-        final Username username = new Username(jwt.value().getName());
-        userSessionService.handleOnMessage(session, username, message);
+    public final void onMessage(Session session, Message message) {
+        validateToken(session)
+                .ifPresent(token -> userSessionService.handleOnMessage(session, new Username(token.getName()), message));
     }
 
     @OnClose
     public final void onClose(Session session) {
-        final Result<JsonWebToken, Throwable> jwt = jwtUtility.extractJWT(session);
-        if (validateToken(session, jwt)) {
-            return;
-        }
-
-        final Username username = new Username(jwt.value().getName());
-        userSessionService.handleOnClose(session, username);
+        validateToken(session)
+                .ifPresent(token -> userSessionService.handleOnClose(session, new Username(token.getName())));
     }
 
-    private static boolean validateToken(Session session, Result<JsonWebToken, Throwable> jwt) {
-        if (!jwt.success()) {
-            WSUtilities.sendMessage(session, "Token is required.");
-            WSUtilities.closeSession(session, "You are don`t authorized.");
-            return true;
+    private Optional<JsonWebToken> validateToken(Session session) {
+        final Optional<JsonWebToken> jwt = jwtUtility.extractJWT(session);
+
+        if (jwt.isEmpty()) {
+            sendMessage(session, Message.error("Token is required."));
+            closeSession(session, Message.error("You are not authorized."));
+            return Optional.empty();
         }
-        return false;
+
+        return jwt;
     }
 }
