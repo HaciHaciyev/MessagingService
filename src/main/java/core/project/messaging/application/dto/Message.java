0431a7a159deb54b36f6e2cc3f4f4a0235faa689
HaciHaciyev <gadzievgadzi508@gmail.com>
2024-12-04 01:39:39 +0400

    simple refactor

--- src/main/java/core/project/messaging/application/dto/Message.java
+++ src/main/java/core/project/messaging/application/dto/Message.java
@@ -1,20 +1,15 @@
 package core.project.messaging.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import core.project.messaging.infrastructure.utilities.json.JsonUtilities;
-import io.quarkus.logging.Log;
 
 import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record Message(MessageType type, String message, String partner) {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
-
     public Message  {
-        Objects.requireNonNull(type, "type cannot be null");
+        Objects.requireNonNull(type, "Type cannot be null.");
     }
 
     public static Message error(String message) {
@@ -29,19 +24,10 @@ public record Message(MessageType type, String message, String partner) {
         return new Message(MessageType.USER_INFO, message, null);
     }
 
-
     @Override
     public String toString() {
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        try {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            Log.error(e);
-        }
-        return "";
+        return JsonUtilities.writeMessage(this);
     }
-
 
     public String asJSON() {
         return JsonUtilities.writeJSON(this);
--- src/main/java/core/project/messaging/infrastructure/utilities/json/JsonUtilities.java
+++ src/main/java/core/project/messaging/infrastructure/utilities/json/JsonUtilities.java
@@ -1,12 +1,10 @@
 package core.project.messaging.infrastructure.utilities.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.project.messaging.application.dto.Message;
-import core.project.messaging.application.dto.MessageType;
-import core.project.messaging.domain.value_objects.Username;
-import core.project.messaging.infrastructure.utilities.containers.Result;
+import io.quarkus.logging.Log;
 
 
 public class JsonUtilities {
@@ -15,11 +13,25 @@ public class JsonUtilities {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    static {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
     public static String writeJSON(Message message) {
         try {
             return objectMapper.writeValueAsString(message);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static String writeMessage(Message message) {
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(message);
+        } catch (JsonProcessingException e) {
+            Log.errorf("Can`t serialize message: %s", e.getMessage());
         }
 
         return "";
