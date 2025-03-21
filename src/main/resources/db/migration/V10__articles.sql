90de7eab715c20d5308443cb87229439b86756fa
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-03-21 14:06:01 +0400

    Feat: full text search on Articles
    Created new column type of ts_vector for articles & created trigger + function for creating a document after each article inserting or update

--- src/main/resources/db/migration/V10__articles.sql
+++ src/main/resources/db/migration/V10__articles.sql
@@ -7,5 +7,20 @@ CREATE TABLE Article (
     status VARCHAR(9) NOT NULL,
     creation_date TIMESTAMP NOT NULL,
     last_updated TIMESTAMP NOT NULL,
+    search_document ts_vector,
     PRIMARY KEY (id)
 );
+
+CREATE INDEX article_search_idx ON Article USING GIN(search_document);
+
+CREATE FUNCTION update_search_document() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.search_document := to_tsvector('english', NEW.header || ' ' || NEW.summary || ' ' || NEW.body);
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER article_search_update
+    BEFORE INSERT OR UPDATE ON Article
+        FOR EACH ROW
+        EXECUTE FUNCTION update_search_document();
