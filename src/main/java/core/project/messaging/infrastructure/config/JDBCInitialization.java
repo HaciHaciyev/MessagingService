88015551165cffe7a47f44a35a44e9ef292828f9
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-04-23 01:04:17 +0400

    config for JDBC initialization

---
+++ src/main/java/core/project/messaging/infrastructure/config/JDBCInitialization.java
@@ -0,0 +1,25 @@
+package core.project.messaging.infrastructure.config;
+
+import com.hadzhy.jdbclight.jdbc.JDBC;
+import io.quarkus.runtime.Startup;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+
+import javax.sql.DataSource;
+
+@Startup
+@ApplicationScoped
+public class JDBCInitialization {
+
+    private final DataSource dataSource;
+
+    public JDBCInitialization(Instance<DataSource> dataSource) {
+        this.dataSource = dataSource.get();
+    }
+
+    @PostConstruct
+    public void init() {
+        JDBC.init(dataSource);
+    }
+}
