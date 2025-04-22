c72a43b00bf01439528100bc00bd75ef1272ef79
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-04-22 23:09:50 +0400

    removed flyway

--- src/main/resources/db/migration/V5__chess-game-history.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ChessGameHistory (
-    id CHAR(36) NOT NULL,
-    chess_game_id CHAR(36) NOT NULL,
-    pgn_chess_representation VARCHAR NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT board_game_fk FOREIGN KEY (chess_game_id) REFERENCES ChessGame(id)
-);
