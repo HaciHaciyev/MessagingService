006388cc81696a2696375c6e962cf6a532d7873e
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-05-04 21:58:17 +0400

    minor fix

--- src/main/java/core/project/messaging/application/service/UserSessionService.java
+++ src/main/java/core/project/messaging/application/service/UserSessionService.java
@@ -50,7 +50,7 @@ public class UserSessionService {
 
         sessionStorage.put(session, Objects.requireNonNull(account.orElseThrow()));
 
-        sendMessage(session, Message.error("Successful connection to messaging"));
+        sendMessage(session, Message.info("Successful connection to messaging"));
         partnershipsService
                 .getAll(username.username())
                 .forEach((user, message) ->
