1796a35b77d6fddf1b65aa4f35c57da83dacd995
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-16 17:18:09 +0400

    Use custom exception in ExceptionHandler

--- src/main/java/core/project/messaging/infrastructure/exceptions/ExceptionHandler.java
+++ src/main/java/core/project/messaging/infrastructure/exceptions/ExceptionHandler.java
@@ -1,14 +1,15 @@
 package core.project.messaging.infrastructure.exceptions;
 
+import core.project.messaging.domain.commons.exceptions.DomainValidationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class ExceptionHandler implements ExceptionMapper<IllegalArgumentException> {
+public class ExceptionHandler implements ExceptionMapper<DomainValidationException> {
 
   @Override
-  public Response toResponse(IllegalArgumentException e) {
+  public Response toResponse(DomainValidationException e) {
     return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
   }
 }
